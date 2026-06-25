f2f54da9de3be034d537b4f18e42159c79ad39db
Priyadharshini <priyadharshinispd39@gmail.com>
2026-06-25 14:47:23 +0530

    holiday

--- src/Day6/classtask/daylp.java
+++ src/Day6/classtask/daylp.java
@@ -4,6 +4,7 @@ public class daylp {
     public static void main(String[] args){
         int count=0;
         for(int month=1;month<=12;month++){
+            //version2
             for(int week=1;week<=4;week++){
                 for(int day=1;day<=7;day++){
                     System.out.println("month :"+month+" week :"+week+" day :"+day);
