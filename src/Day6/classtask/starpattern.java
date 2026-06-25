03280d8d869961bdf7569f9fbe615d845ee72ee9
Priyadharshini <priyadharshinispd39@gmail.com>
2026-06-25 15:18:13 +0530

    1*to5*

--- src/Day6/classtask/starpattern.java
+++ src/Day6/classtask/starpattern.java
@@ -3,7 +3,7 @@ package Day6.classtask;
 public class starpattern {
     public static void main(String[] args){
         for(int i=1;i<=5;i++){
-            for(int j=1;j<=5;j++){
+            for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();
