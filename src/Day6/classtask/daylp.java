6118dd3bdd9201076e3e306458dd3a0cc71e2113
Priyadharshini <priyadharshinispd39@gmail.com>
2026-06-25 14:15:20 +0530

    dwm 1 holiday

--- src/Day6/classtask/daylp.java
+++ src/Day6/classtask/daylp.java
@@ -2,13 +2,19 @@ package Day6.classtask;
 
 public class daylp {
     public static void main(String[] args){
+        int count=0;
         for(int month=1;month<=12;month++){
             for(int week=1;week<=4;week++){
                 for(int day=1;day<=7;day++){
-                    System.out.println("month :"+month+" week :"+week+" day :"+day);
-                    System.out.println("come to clg and sty and go");
+                    if(day==1){
+                       System.out.println("month :"+month+" week :"+week+" day :"+day);
+                       System.out.println("sunday holiday");
+                        count=day+count;
+                    }
+
                 }
             }
         }
+        System.out.println("total days :" + count);
     }
 }
