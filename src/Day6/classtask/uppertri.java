259c24d85c4c31df9386743d9c063a4170ef8d86
Priyadharshini <priyadharshinispd39@gmail.com>
2026-06-25 14:49:50 +0530

    1to12345

---
+++ src/Day6/classtask/uppertri.java
@@ -0,0 +1,12 @@
+package Day6.classtask;
+
+public class uppertri {
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++){//rows
+            for(int j=1;j<=i;j++){//coloumn
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
