8ea62ed7b96f52855e79a984e454f7504ad4cdfe
Priyadharshini <priyadharshinispd39@gmail.com>
2026-06-27 14:05:58 +0530

    getmarksArr

---
+++ src/day7/classtask/lenth.java
@@ -0,0 +1,9 @@
+package day7.classtask;
+
+public class lenth {
+    public static void main(String[] args){
+        int [] arr={1,2,3,4,5,6};
+        int d=arr.length;
+        System.out.println(d);
+    }
+}
